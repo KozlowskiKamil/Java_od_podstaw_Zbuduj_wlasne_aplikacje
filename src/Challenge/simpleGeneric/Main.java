ba9c6ddfed04a78b6cd7f257f9a4de0e176ca5a8
KozlowskiKamil <kamil2k8@gmail.com>
2023-03-31 09:43:56 +0200

    End all string lesson go to OOP

---
+++ src/Challenge/simpleGeneric/Main.java
@@ -0,0 +1,7 @@
+package Challenge.simpleGeneric;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/example/oop/basic/FinalConstantsExample.java
+++ src/com/example/oop/basic/FinalConstantsExample.java
@@ -1,5 +1,7 @@
 package com.example.oop.basic;
 
+class PointWithConstants{
+}
 public class FinalConstantsExample {
     public static void main(String[] args) {
         // TODO: 30.03.2023 ćwiczenie 95 kontynuacja
