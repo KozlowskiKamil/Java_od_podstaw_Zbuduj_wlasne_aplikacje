29c3dbe6dd02cfc723850ef149e94504f0195656
KozlowskiKamil <kamil2k8@gmail.com>
2023-04-03 19:34:33 +0200

    Add stream methods

--- src/com/example/streams/OptionalCreateOfNullableEmpty.java
+++ src/com/example/streams/OptionalCreateOfNullableEmpty.java
@@ -3,5 +3,7 @@ package com.example.streams;
 public class OptionalCreateOfNullableEmpty {
     public static void main(String[] args) {
         // TODO: 02.04.2023 kontynuacja stream optional lekcja 185 od początki
+
+
     }
 }
