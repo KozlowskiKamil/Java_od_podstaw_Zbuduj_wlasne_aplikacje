b22485195b81cec51fd97937f69c687f8dec587c
KozlowskiKamil <kamil2k8@gmail.com>
2023-04-08 22:37:39 +0200

    challenge ok

--- src/Challenge/itemChallenge/Main.java
+++ src/Challenge/itemChallenge/Main.java
@@ -1,5 +1,41 @@
 package Challenge.itemChallenge;
+// TODO: 08.04.2023 zrobione, ale do powtórzenia
+
+class Item {
+    static int numItems = 0;
+    final int ID;
+
+    public Item(int id) {
+        this.ID = id;
+    }
+
+    static int getNextId() {
+        return Item.numItems++;
+    }
+
+    static Item getItem() {
+        int id = Item.getNextId();
+        Item item = new Item(id);
+        return item;
+    }
+
+    static void printNumItems() {
+        System.out.println("numItems = " + Item.numItems);
+    }
+
+    public void printId() {
+        System.out.println("ID = " + this.ID);
+    }
+
+}
 
 public class Main {
-    // TODO: 31.03.2023 Ćwiczenie 98 zadanie static
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Item item = Item.getItem();
+            item.printId();
+        }
+        Item.printNumItems();
+    }
+
 }
--- src/Challenge/truck2Challenge/Main.java
+++ src/Challenge/truck2Challenge/Main.java
@@ -1,7 +1,34 @@
 package Challenge.truck2Challenge;
 
+
+class Truck {
+    String producent, model, kolor;
+    int iloscKol;
+    final int TOP_SPEED = 100;
+    final int ACCELERATION;
+
+    public Truck() {
+        producent = "Freightliner";
+        model = "9664";
+        iloscKol = 6;
+        ACCELERATION = 60;
+    }
+
+    public Truck(String kolor) {
+        this();
+        this.kolor = kolor;
+    }
+    public void printInfo(){
+        System.out.println("producent = " + producent);
+        System.out.println("model = " + model);
+        System.out.println("kolor = " + kolor);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        // TODO: 31.03.2023 Cwiczenie final nr ćwiczenia 96 
+        Truck truck = new Truck("żółty");
+        truck.printInfo();
+
     }
 }
