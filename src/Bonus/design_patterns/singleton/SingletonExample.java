2bee5b50f72ac86dc5b92dd14b764c333e54b7ef
KozlowskiKamil <kamil2k8@gmail.com>
2023-04-09 19:27:14 +0200

    Singleton -

---
+++ src/Bonus/design_patterns/singleton/SingletonExample.java
@@ -0,0 +1,29 @@
+package Bonus.design_patterns.singleton;
+
+class SingletonPattern {
+    private static SingletonPattern instance = new SingletonPattern();
+
+    private SingletonPattern() {
+        // inicjacja połączenia z  baza danych tylko 1 raz
+    }
+
+    public static SingletonPattern getInstance() {
+        return instance;
+    }
+
+    public void getDBConnection() {
+        System.out.println("DB Connection");
+    }
+}
+
+public class SingletonExample {
+    public static void main(String[] args) {
+        SingletonPattern sinhleton1 = SingletonPattern.getInstance();
+        sinhleton1.getDBConnection();
+
+        SingletonPattern sinhleton2 = SingletonPattern.getInstance();
+        sinhleton2.getDBConnection();
+
+        System.out.println(sinhleton1 == sinhleton2);
+    }
+}
--- src/com/example/streams/OptionalCreateOfNullableEmpty.java
+++ src/com/example/streams/OptionalCreateOfNullableEmpty.java
@@ -1,7 +1,34 @@
 package com.example.streams;
 
+import java.util.Optional;
+
 public class OptionalCreateOfNullableEmpty {
     public static void main(String[] args) {
-        // TODO: 02.04.2023 kontynuacja stream optional lekcja 185 od początki
+        // Optional.of() tworzy optional który nie może być null
+
+        Optional<String> opt1 = Optional.of("test");
+        System.out.println("opt1 = " + opt1.get());
+
+        //    Optional<String> opt2 = Optional.of(null); wyrzuci błąd nie może być null
+
+        Optional<String> opt3 = Optional.ofNullable("optional3");
+        System.out.println("opt3 = " + opt3.get()); // optional 3
+
+        Optional<String> opt4 = Optional.ofNullable(null);
+        System.out.println("opt4 = " + opt4); // optional empty
+
+        Optional<String> opt5 = Optional.empty();
+        System.out.println("opt5 = " + opt5); // optional empty
+
+        Integer integer = Integer.valueOf(10);
+      //  integer = null;
+        Optional<Integer> optInt = Optional.ofNullable(integer);
+        if (optInt.isPresent()) {
+            System.out.println("optInt = " + optInt.get());
+        } else System.out.println(" empty ");
+
+        optInt.ifPresent(i -> System.out.println("i = " + i));
+        optInt.ifPresent(System.out::println);
     }
+
 }
